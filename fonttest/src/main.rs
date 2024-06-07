ad9f29ae254908fcef4fec052a797beb27c63cca
jesko7 <119683030+jesko7@users.noreply.github.com>
2024-06-07 19:27:02 +0200

    Add files via upload

---
+++ fonttest/src/main.rs
@@ -0,0 +1,35 @@
+use macroquad::prelude::*;
+
+//mac:                  brew install fontconfig freetype
+//linux:                sudo apt-get install libfontconfig1 libfreetype6
+
+
+#[macroquad::main("Load Font Example")]
+async fn main() {
+    let font_result = load_ttf_font("assets/Roboto-Regular.ttf").await;
+    let font = match font_result {
+        Ok(font) => font,
+        Err(e) => {
+            eprintln!("Failed to load font: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    loop {
+        clear_background(BLACK);
+
+        draw_text_ex(
+            "FUFUFUFUFUFUFUFUFUFU",
+            300.0,
+            300.0,
+            TextParams {
+                font: Some(&font),
+                font_size: 100,
+                color: WHITE,
+                ..Default::default()
+            },
+        );
+
+        next_frame().await;
+    }
+}
